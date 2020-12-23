aebf2cd9b26e490e22cd06c2493c394191079929
dungtc01 <dungtc01@acb.com.vn>
2020-12-23 21:45:59 +0700

    update shell script for standalone and distributed mode

--- connector/distributed.sh
+++ connector/distributed.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-# Standalone mode for develop purpose
-### define a property file and use connect process in command line 
-docker run --rm -it -v $(pwd)/source/standalone:/connectors --net=host landoop/fast-data-dev:2.5.0 bash
-cd connectors
-connect-standalone worker.properties source-connector.properties
-
-# using rest API
-curl -X POST http://localhost:8083/connectors -H "Content-Type: application/json" -d @source/standalone/standalone.json
-
 # # Distributed mode for production
 # avro schema
 curl -X POST http://localhost:8083/connectors -H "Content-Type: application/json" -d @source/distributed/connector-avro.json
